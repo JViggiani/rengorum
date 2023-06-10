4070158d634dfc188949d1db57b7d26765d9cf61
Josh Viggiani <josh.viggiani@gmail.com>
2023-06-10 20:09:08 +0100

    Styling - unfinished

--- frontend/src/components/documenttable/index.js
+++ frontend/src/components/documenttable/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTable } from 'react-table';
 import StatusMessage from '../statusmessage';
+import {Segment, Icon} from 'semantic-ui-react';
+import './styles.css';
 
 function DocumentTable(props) {
     const data = React.useMemo(() => props.data, [props.data]);
@@ -33,29 +35,35 @@ function DocumentTable(props) {
     } = useTable({ columns, data });
 
     return (
-        <table {...getTableProps()}>
-        <thead>
-            {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                ))}
-            </tr>
-            ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-                prepareRow(row);
-                return (
-                    <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => (
-                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+        <div className="document-table-container">
+            <div className="document-table-title">
+                <Icon name={'file outline'} />
+                {"Placeholder - needs to be a title for a world for example"}
+            </div>
+            <table className="document-table" {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
                     ))}
-                    </tr>
-                );
-            })}
-        </tbody>
-        </table>
+                </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        ))}
+                        </tr>
+                    );
+                })}
+            </tbody>
+            </table>
+        </div>
     );
 }
 
